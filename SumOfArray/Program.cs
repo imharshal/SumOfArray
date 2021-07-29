2e479b6a0d5e1d164d9da1aee429780813fb3fa0
Harshal Bokade <harshalbokade984@gmail.com>
2021-07-29 22:04:21 +0530

    Add project files.

---
+++ SumOfArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int sum=0;
+            Console.WriteLine("Enter 10 numbers in array");
+            for(int i = 0; i<10; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach(int i in arr)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine("Sum of array: {0}", sum);
+        }
+    }
+}
